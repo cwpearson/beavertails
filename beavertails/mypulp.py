0585e01540d229ab9820e3feebb8f7375dcaf27b
Carl Pearson <me@carlpearson.net>
2024-01-17 15:38:00 -0700

    delete_on_close=False for temporary file

--- beavertails/mypulp.py
+++ beavertails/mypulp.py
@@ -4,7 +4,7 @@ from pulp.apis import PULP_CBC_CMD
 
 
 def solve(prob):
-    with NamedTemporaryFile() as log_file:
+    with NamedTemporaryFile(delete_on_close=False) as log_file:
         status = prob.solve(PULP_CBC_CMD(msg=False, logPath=log_file.name))
         log_file.seek(0)
         log = log_file.read().decode("utf-8")
