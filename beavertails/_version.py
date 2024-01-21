c9f216678d3a2119f5df9fca1c1a184b78fd5be9
Carl Pearson <me@carlpearson.net>
2024-01-21 13:57:54 -0700

    CI: invoke version information script

--- beavertails/_version.py
+++
@@ -1,3 +0,0 @@
-GIT_TAG = "v0.2.2"
-GIT_BRANCH = "master"
-GIT_SHA = "d3044d1"
--- scripts/make-version.sh
+++ scripts/make-version.sh
@@ -7,6 +7,6 @@ GIT_BRANCH=$(git symbolic-ref -q --short HEAD)
 GIT_SHA=$(git rev-parse --short HEAD)
 
 
-echo "GIT_TAG=\"$GIT_TAG\"">> $SCRIPTPATH/../beavertails/_version.py
-echo "GIT_BRANCH=\"$GIT_BRANCH\"">> $SCRIPTPATH/../beavertails/_version.py
-echo "GIT_SHA=\"$GIT_SHA\"">> $SCRIPTPATH/../beavertails/_version.py
+echo "GIT_TAG=\"$GIT_TAG\"" > $SCRIPTPATH/../beavertails/_version.py
+echo "GIT_BRANCH=\"$GIT_BRANCH\"" >> $SCRIPTPATH/../beavertails/_version.py
+echo "GIT_SHA=\"$GIT_SHA\"" >> $SCRIPTPATH/../beavertails/_version.py
